2e0dd001126a6eb93f28590cb1a40396ebdaa044
stefanoszormpas <64742717+stefanoszormpas@users.noreply.github.com>
2026-05-15 08:55:36 +0200

    Update quiz.js

--- assets/quiz.js
+++ assets/quiz.js
@@ -37,7 +37,8 @@ document.addEventListener('DOMContentLoaded', async function() {
         "26":3000,
         "27":3000,
         "28":3000,
-        "29":3000
+        "29":3000,
+        "30":3000
     };
 
     let initialTime = quizTimes[quizId] || 600;
